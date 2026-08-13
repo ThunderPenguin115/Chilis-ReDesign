295337812a73e50e4b99c781e43df93b979175c8
Matthew-Bueno <Matthew.bueno09@gmail.com>
2026-08-13 07:58:34 -0700

    Update menu.js
    Added event listeners for breakfast, lunch, and dinner to control visibility of menu items.

--- data/menu.js
+++ data/menu.js
@@ -6,3 +6,33 @@ const menuItems = [
         image : "assets/tD.avif"
     }
 ];
+
+const breakfast = document.getElementById("breakfast");
+const lunch = document.getElementById("lunch");
+const dinner = document.getElementById("dinner");
+
+const cards = document.querySelectorAll(".card");
+
+breakfast.addEventListener("click", function(){
+    cards.forEach(function(card){
+        card.querySelectorAll("h1, p, img, #price").forEach(function(text){
+            text.style.visibility = "hidden";
+        });
+    });
+});
+
+dinner.addEventListener("click", function(){
+    cards.forEach(function(card){
+        card.querySelectorAll("h1, p, img, #price").forEach(function(text){
+            text.style.visibility = "hidden";
+        });
+    });
+});
+
+lunch.addEventListener("click", function(){
+    cards.forEach(function(card){
+        card.querySelectorAll("h1, p, img, #price").forEach(function(text){
+            text.style.visibility = "visible";
+        });
+    });
+});
